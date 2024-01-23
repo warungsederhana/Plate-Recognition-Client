930a32c8e44d87caed54bab45096f0bd71e8728d
gybran <charistomarcgybran@students.undip.ac.id>
2024-01-23 19:00:00 +0700

    feat: login process (sampai console.log token)

--- src/lib/firebase/services.ts
+++ src/lib/firebase/services.ts
@@ -1,5 +1,11 @@
 import app from "./init";
-import { getAuth, createUserWithEmailAndPassword, UserCredential, AuthError } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  UserCredential,
+  AuthError,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 export const register = async (
   email: string,
@@ -18,6 +24,30 @@ export const register = async (
       email,
       password
     );
+    const user = userCredential.user;
+    const token = await user.getIdToken();
+    console.log(`Token: ${token}`);
+  } catch (error) {
+    // Assuming error is of type FirebaseError, which is typically the case with Firebase operations
+    if (error instanceof Error) {
+      const errorCode = (error as AuthError).code;
+      const errorMessage = error.message;
+      console.error(`Error Code: ${errorCode}, Message: ${errorMessage}`);
+      // Consider re-throwing the error or handling it according to your application's needs
+      throw error;
+    } else {
+      // Handle unexpected errors that are not Error instances
+      console.error("An unexpected error occurred", error);
+      throw new Error("An unexpected error occurred");
+    }
+  }
+};
+
+export const login = async (email: string, password: string): Promise<void> => {
+  const auth = getAuth(app);
+
+  try {
+    const userCredential: UserCredential = await signInWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
     const token = await user.getIdToken();
     console.log(`Token: ${token}`);
